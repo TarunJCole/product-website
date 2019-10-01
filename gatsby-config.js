c08911e996f67c1b5aed9f3d61e52a49a4d7cc3a
TarunJCole <elocnurat@hotmail.com>
2019-10-01 15:19:54 +0100

    name & desc

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `AndroPhone`,
+    description: `The latest and only phone you will ever need.`,
+    author: `Tarun Cole`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `AndroPhone`,
+        short_name: `Phone`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
