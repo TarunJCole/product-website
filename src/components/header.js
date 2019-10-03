41eeb479f79a1d01f7f3f5f72dd98dbee9a35030
TarunJCole <elocnurat@hotmail.com>
2019-10-03 16:18:44 +0100

    styling + icons

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,38 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { IconContext } from "react-icons"
+import { FaFacebookSquare, FaTwitterSquare } from "react-icons/fa"
 
 import headerStyles from "./header.module.css"
 
 const Header = ({ siteTitle }) => (
   <header style={headerStyles}>
     <nav>
-      <ul>
-        <li>Contact</li>
-      </ul>
+      <div>
+        <ul>
+          <li>Contact</li>
+        </ul>
+      </div>
       <div>
         <h2>{siteTitle}</h2>
       </div>
       <div>
         <ul>
-          <li>item</li>
-          <li>item</li>
+          <li>
+            <IconContext.Provider
+              value={{ size: "2rem", style: { marginRight: "0.4rem" } }}
+            >
+              <FaFacebookSquare /> <span>Facebook</span>
+            </IconContext.Provider>
+          </li>
+          <li>
+            <IconContext.Provider
+              value={{ size: "2rem", style: { marginRight: "0.4rem" } }}
+            >
+              <FaTwitterSquare /> <span>Twitter</span>
+            </IconContext.Provider>
+          </li>
         </ul>
       </div>
     </nav>
