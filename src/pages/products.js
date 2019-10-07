572c9ea429d6e319fd41ee27b1695e6c9f1b46ab
TarunJCole <elocnurat@hotmail.com>
2019-10-07 14:12:17 +0100

    create

---
+++ src/pages/products.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default function Products() {
+  return (
+    <Layout>
+      <SEO title="Products" />
+      <section className="products">
+        <h1>Products</h1>
+      </section>
+    </Layout>
+  )
+}
